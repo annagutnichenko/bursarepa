07e7807a9d6be0203464663fffd8c4fab63f9c12
Anna <anna.gutnichenko@betlab.com>
2016-03-05 22:43:09 +0200

    userscontrollers

--- Petitions/app/controllers/users_controller.rb
+++ Petitions/app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def index
   end
-end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+
+    @user = User.new(user_params)
+
+    if @user.save
+
+      redirect_to root_url, notice: "Спасибо за регистрацию!"
+
+    else
+
+      render "new"
