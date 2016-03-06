799442a4a693c987f35debe3bb2b119c8e83cd63
Anna <anna.gutnichenko@betlab.com>
2016-03-06 13:37:43 +0200

    something

--- Petitions/app/models/user.rb
+++ Petitions/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  validates :email, presence: true
+  has_secure_password
+
+  attr_accessible :email, :password, :password_confirmation
+
+  validates_uniqueness_of :email
 end
