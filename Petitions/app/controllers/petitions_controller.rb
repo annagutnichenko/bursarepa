767909e687c4c2f23f3f1f555038f2cdee1608c2
Anna <anna.gutnichenko@betlab.com>
2016-03-21 12:39:24 +0200

    homework 7 part

--- Petitions/app/controllers/petitions_controller.rb
+++ Petitions/app/controllers/petitions_controller.rb
@@ -13,4 +13,34 @@ class PetitionsController < ApplicationController
     @petition = Petition.find(params[:id])
   end
 
+  def create
+    petition = current_user.petitions.create(permitted_params)
+    redirect_to petition
   end
+
+  def new
+    @petition = current_user.petitions.new
+  end
+
+  def edit
+    @petition = current_user.petitions.find(params[:id])
+  end
+
+  def update
+    petition = current_user.petitions.find(params[:id])
+    petition.update!(permitted_params)
+    redirect_to petition, notice: 'Петиция обновлена'
+  end
+
+  def destroy
+    petition = current_user.petitions.find(params[:id])
+    petition.destroy
+    redirect_to action: :index, notice: 'Петиция удалена'
+  end
+
+  private
+
+  def permitted_params
+    params.require(:petition).permit(:title, :description)
+  end
+end
